15b85ce9a78c903cd8019eb3c035c36ea258dd4e
Елшин Антон <aelshin@diasoft.ru>
2021-01-28 14:53:32 +0300

    MSA_Task_13 - Проектная работа

--- MSA_Task_13/demo/service/src/main/java/ru/diasoft/micro/config/ServletInitializer.java
+++ MSA_Task_13/demo/service/src/main/java/ru/diasoft/micro/config/ServletInitializer.java
@@ -3,13 +3,13 @@ package ru.diasoft.micro.config;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import ru.diasoft.micro.DemoApplication;
+import ru.diasoft.micro.TaskApplication;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
+        return application.sources(TaskApplication.class);
     }
 
 }
